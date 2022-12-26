5636ce5095d89adabac2562bb380df16af6cc79c
bi537lgya <bi537lgya@login01.sc.uni-leipzig.de>
2022-12-26 22:26:13 +0100

    added parallel rendering

--- render_shapenet_data/render_relief.py
+++ render_shapenet_data/render_relief.py
@@ -8,7 +8,23 @@
 
 import os
 import argparse
+from multiprocessing.pool import ThreadPool
 
+def blender_job(blender_root, save_folder, dataset_folder, synset, file, obj_scale, views):
+    render_cmd = '%s -b -P render_shapenet.py -- --output %s %s  --scale %f --views %d --resolution 1024 >> tmp.out' % (
+        blender_root, 
+        save_folder, 
+        os.path.join(dataset_folder, synset, file, 'model.obj'), 
+        obj_scale, 
+        views)
+    os.system(render_cmd)
+
+synset_list = [
+    'Version1'  # Gray_Checkerboard
+]
+scale_list = [
+    0.8
+]
 parser = argparse.ArgumentParser(description='Renders given obj file by rotation a camera around it.')
 parser.add_argument(
     '--save_folder', type=str, default='./tmp',
@@ -22,6 +38,9 @@ parser.add_argument(
 parser.add_argument(
     '--views', type=int, default=24,
     help='number of views')
+parser.add_argument(
+    '--worker', type=int, default=1,
+    help='number of blender-workers')
 args = parser.parse_args()
 
 save_folder = args.save_folder
@@ -29,16 +48,11 @@ dataset_folder = args.dataset_folder
 blender_root = args.blender_root
 views = args.views
 
-synset_list = [
-    'Version2'  # Gray_Checkerboard
-]
-scale_list = [
-    0.8
-]
+tp = ThreadPool(args.worker)
 for synset, obj_scale in zip(synset_list, scale_list):
     file_list = sorted(os.listdir(os.path.join(dataset_folder, synset)))
     for idx, file in enumerate(file_list):
-        render_cmd = '%s -b -P render_shapenet.py -- --output %s %s  --scale %f --views %d --resolution 1024 >> tmp.out' % (
-            blender_root, save_folder, os.path.join(dataset_folder, synset, file, 'model.obj'), obj_scale, views,
-        )
-        os.system(render_cmd)
+        tp.apply_async(blender_job, (blender_root, save_folder, dataset_folder, synset, file, obj_scale, views))
+
+tp.close()
+tp.join()
